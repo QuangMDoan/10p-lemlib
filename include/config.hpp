1f39dfe1d0e7b065e67ea5f8be491a06aa7dd3a5
FusingChart <rishraowork@gmail.com>
2026-03-01 21:07:17 -0800

    more skills tuning, made mid goal way more consistent with unjams

--- include/config.hpp
+++ include/config.hpp
@@ -47,7 +47,6 @@ inline lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
 inline pros::Imu imu(13);
 
 // Distance sensors for DSR (Distance Sensor Reset)
-// Note: Some may not be wired yet
 inline pros::Distance left_distance_sensor(4);     // left side distance sensor
 inline pros::Distance right_distance_sensor(20);   // right side distance sensor
 inline pros::Distance front_left_distance_sensor(7);   // front left distance sensor
@@ -82,7 +81,7 @@ inline lemlib::ControllerSettings angular_controller(2, // proportional gain (kP
                                               0, // integral gain (kI)
                                               10, // derivative gain (kD)
                                               3, // anti windup
-                                              1, // small error range, in degrees
+                                              2, // small error range, in degrees
                                               100, // small error range timeout, in milliseconds
                                               3, // large error range, in degrees
                                               500, // large error range timeout, in milliseconds
