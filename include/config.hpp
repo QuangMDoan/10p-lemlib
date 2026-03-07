57f7540fc3b9a0356a49001801f3fa4b5c89548d
FusingChart <rishraowork@gmail.com>
2026-03-06 20:25:24 -0800

    made changes for rewiring and tuning mid goal

--- include/config.hpp
+++ include/config.hpp
@@ -30,15 +30,15 @@ inline pros::adi::DigitalOut stopper('C'); // expansion solenoid
 // left motor group
 inline pros::MotorGroup left_motor_group({-14, -15, -16}, pros::MotorGears::blue);
 // right motor group
-inline pros::MotorGroup right_motor_group({12, 18, 19}, pros::MotorGears::blue);
+inline pros::MotorGroup right_motor_group({12, 18, 11}, pros::MotorGears::blue);
 
 // drivetrain settings
-inline lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
-                              &right_motor_group, // right motor group
-                              11.5, // 10 inch track width
-                              lemlib::Omniwheel::NEW_325, // using new 4" omnis
-                              450, // drivetrain rpm is 360
-                              2 // horizontal drift is 2 (for now)
+inline lemlib::Drivetrain drivetrain(&left_motor_group,          // left motor group
+                                     &right_motor_group,         // right motor group
+                                     11.5,                       // 10 inch track width
+                                     lemlib::Omniwheel::NEW_325, // using new 4" omnis
+                                     450,                        // drivetrain rpm is 360
+                                     2                           // horizontal drift is 2 (for now)
 );
 
 // ============ SENSORS ============
@@ -47,54 +47,54 @@ inline lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
 inline pros::Imu imu(13);
 
 // Distance sensors for DSR (Distance Sensor Reset)
-inline pros::Distance left_distance_sensor(4);     // left side distance sensor
-inline pros::Distance right_distance_sensor(20);   // right side distance sensor
-inline pros::Distance front_left_distance_sensor(7);   // front left distance sensor
-inline pros::Distance front_right_distance_sensor(6);  // front right distance sensor
+inline pros::Distance left_distance_sensor(4);        // left side distance sensor
+inline pros::Distance right_distance_sensor(20);      // right side distance sensor
+inline pros::Distance front_left_distance_sensor(7);  // front left distance sensor
+inline pros::Distance front_right_distance_sensor(6); // front right distance sensor
 
 // ============ ODOMETRY ============
 
 // odometry settings
 inline lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1
-                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
-                            nullptr, // horizontal tracking wheel 1
-                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
-                            &imu // inertial sensor
+                                   nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                                   nullptr, // horizontal tracking wheel 1
+                                   nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                                   &imu     // inertial sensor
 );
 
 // ============ PID CONTROLLERS ============
 
 // lateral PID controller
-inline lemlib::ControllerSettings lateral_controller(9, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              40, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
-                                              500, // large error range timeout, in milliseconds
-                                              20 // maximum acceleration (slew)
+inline lemlib::ControllerSettings lateral_controller(9,   // proportional gain (kP)
+                                                     0,   // integral gain (kI)
+                                                     40,  // derivative gain (kD)
+                                                     3,   // anti windup
+                                                     1,   // small error range, in inches
+                                                     100, // small error range timeout, in milliseconds
+                                                     3,   // large error range, in inches
+                                                     500, // large error range timeout, in milliseconds
+                                                     20   // maximum acceleration (slew)
 );
 
 // angular PID controller
-inline lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              10, // derivative gain (kD)
-                                              3, // anti windup
-                                              2, // small error range, in degrees
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in degrees
-                                              500, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
+inline lemlib::ControllerSettings angular_controller(2,   // proportional gain (kP)
+                                                     0,   // integral gain (kI)
+                                                     10,  // derivative gain (kD)
+                                                     3,   // anti windup
+                                                     2,   // small error range, in degrees
+                                                     100, // small error range timeout, in milliseconds
+                                                     3,   // large error range, in degrees
+                                                     500, // large error range timeout, in milliseconds
+                                                     0    // maximum acceleration (slew)
 );
 
 // ============ CHASSIS ============
 
 // create the chassis
-inline lemlib::Chassis chassis(drivetrain, // drivetrain settings
-                        lateral_controller, // lateral PID settings
-                        angular_controller, // angular PID settings
-                        sensors // odometry sensors
+inline lemlib::Chassis chassis(drivetrain,         // drivetrain settings
+                               lateral_controller, // lateral PID settings
+                               angular_controller, // angular PID settings
+                               sensors             // odometry sensors
 );
 
 #endif // CONFIG_HPP
--- include/intake_manager.hpp
+++ include/intake_manager.hpp
@@ -3,7 +3,8 @@
 
 #include "config.hpp"
 
-enum class IntakeState {
+enum class IntakeState
+{
 	IDLE,
 	SCORE_LOW,
 	SCORE_MID,
@@ -15,21 +16,25 @@ enum class IntakeState {
 	SCORE_MID_FAST
 };
 
-class IntakeManager {
+class IntakeManager
+{
 private:
 	IntakeState current_state = IntakeState::IDLE;
 	uint32_t last_high_goal_time = 0;
 
 public:
-	IntakeManager() {
+	IntakeManager()
+	{
 		// Set all motors to hold
 		intake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 		middle_roller.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 		top_roller.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 	}
 
-	void set_state(IntakeState state) {
-		if (current_state == IntakeState::SCORE_HIGH && state != IntakeState::SCORE_HIGH) {
+	void set_state(IntakeState state)
+	{
+		if (current_state == IntakeState::SCORE_HIGH && state != IntakeState::SCORE_HIGH)
+		{
 			last_high_goal_time = pros::millis();
 		}
 
@@ -37,81 +42,85 @@ public:
 		update();
 	}
 
-	IntakeState get_state() const {
+	IntakeState get_state() const
+	{
 		return current_state;
 	}
 
-	void update() {
+	void update()
+	{
 
 		bool in_cooldown = (pros::millis() - last_high_goal_time) < 250;
 
-		switch (current_state) {
-			case IntakeState::IDLE:
-				stopper.set_value(in_cooldown ? false : true);
-				intake.move(0);
-				middle_roller.move(0);
-				top_roller.move(0);
-				break;
+		switch (current_state)
+		{
+		case IntakeState::IDLE:
+			stopper.set_value(in_cooldown ? false : true);
+			intake.move(0);
+			middle_roller.move(0);
+			top_roller.move(0);
+			break;
 
-			case IntakeState::SCORE_LOW:
-				// all rollers run reversed
-				stopper.set_value(in_cooldown ? false : true);
-				intake.move(-127);
-				middle_roller.move(-127);
-				top_roller.move(-127);
-				break;
+		case IntakeState::SCORE_LOW:
+			// all rollers run reversed
+			stopper.set_value(in_cooldown ? false : true);
+			intake.move(-127);
+			middle_roller.move(-127);
+			top_roller.move(-127);
+			break;
 
-			case IntakeState::SCORE_MID:
-				// intake and middle roller run forward, top roller runs reversed
-				stopper.set_value(in_cooldown ? false : true);
-				intake.move(127);
-				middle_roller.move(127);
-				top_roller.move(-67);
-				break;
+		case IntakeState::SCORE_MID:
+			// intake and middle roller run forward, top roller runs reversed
+			stopper.set_value(in_cooldown ? false : true);
+			intake.move(127);
+			middle_roller.move(127);
+			top_roller.move(-67);
+			break;
 
-			case IntakeState::SCORE_HIGH:
-				// all rollers run forward
-				stopper.set_value(false);
-				intake.move(127);
-				middle_roller.move(127);
-				top_roller.move(127);
-				break;
+		case IntakeState::SCORE_HIGH:
+			// all rollers run forward
+			stopper.set_value(false);
+			intake.move(127);
+			middle_roller.move(127);
+			top_roller.move(127);
+			break;
 
-			case IntakeState::INTAKE:
-				// intake and middle roller run forward, top roller is idle
-				stopper.set_value(true);
-				intake.move(127);
-				middle_roller.move(127);
-				top_roller.move(33);
-				break;
+		case IntakeState::INTAKE:
+			// intake and middle roller run forward, top roller is idle
+			stopper.set_value(true);
+			intake.move(127);
+			middle_roller.move(127);
+			top_roller.move(33);
+			break;
 
-			case IntakeState::SCORE_MID_SKILLS:
-				// intake and middle roller run forward, top roller runs reversed
-				stopper.set_value(in_cooldown ? false : true);
-				intake.move(127);
-				middle_roller.move(40);
-				top_roller.move(-30);
-				break;
+		case IntakeState::SCORE_MID_SKILLS:
+			// intake and middle roller run forward, top roller runs reversed
+			stopper.set_value(in_cooldown ? false : true);
+			intake.move(127);
+			middle_roller.move(40);
+			top_roller.move(-25);
+			break;
 
-			case IntakeState::SCORE_LOW_SKILLS:
-				// all rollers run reversed
-				stopper.set_value(in_cooldown ? false : true);
-				intake.move(-70);
-				middle_roller.move(-127);
-				top_roller.move(-127);
-				break;
+		// TODO: tune with pistonized intake once built
+		case IntakeState::SCORE_LOW_SKILLS:
+			// all rollers run reversed
+			stopper.set_value(in_cooldown ? false : true);
+			intake.move(-70);
+			middle_roller.move(-127);
+			top_roller.move(-127);
+			break;
 
-			case IntakeState::UNJAM:
-				intake.move(-127);
-				middle_roller.move(-127);
-				break;	
-			
-			case IntakeState::SCORE_MID_FAST:
-				stopper.set_value(in_cooldown ? false : true);
-				intake.move(127);
-				middle_roller.move(127);
-				top_roller.move(-127);
-				break;
+		case IntakeState::UNJAM:
+			intake.move(-127);
+			middle_roller.move(-127);
+			break;
+
+		case IntakeState::SCORE_MID_FAST:
+			stopper.set_value(in_cooldown ? false : true);
+			intake.move(127);
+			middle_roller.move(127);
+			top_roller.move(-127);
+			break;
 		}
 	}
 };
